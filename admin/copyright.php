f1cee964681759f0991d24d4a9ef943298f9ad0e
GARFIELDTOM <coolerxde@gt.ac.cn>
2026-09-03 19:51:53 +0800

    【语言】版权等页面支持多语言

--- admin/copyright.php
+++ admin/copyright.php
@@ -12,23 +12,23 @@
             <a href="https://github.com/little-gt/BooAdmin" class="font-medium hover:text-discord-accent transition-colors" target="_blank" rel="noopener">BooAdmin</a>
             <div class="booadmin-copyright-popup" id="booadminCopyrightPopup">
                 <button class="close-btn" onclick="closePopup(); event.stopPropagation();">&times;</button>
-                <h3>关于 BooAdmin 程序</h3>
-                <div class="version">版本 <?php echo BOOADMIN_VERSION; ?></div>
+                <h3><?php _e('关于 BooAdmin 程序'); ?></h3>
+                <div class="version"><?php _e('版本'); ?> <?php echo BOOADMIN_VERSION; ?></div>
                 <div class="content">
                     <div class="left">
-                        <p class="main-copy"><strong>BooAdmin 是免费开源项目。</strong>BooAdmin 的开源维护、CDN资源分发与新功能更新都离不开您的捐助。您的支持将帮助我覆盖以下成本：</p>
+                        <p class="main-copy"><strong><?php _e('BooAdmin 是免费开源项目。'); ?></strong><?php _e('BooAdmin 的开源维护、CDN资源分发与新功能更新都离不开您的捐助。您的支持将帮助我覆盖以下成本：'); ?></p>
                         <ul class="support-points">
-                            <li><strong>开源维护成本：</strong>进行版本适配、问题修复、体验优化与 LTS 支持。</li>
-                            <li><strong>服务运行成本：</strong>提供静态资源分发、国际/国内线路与 IPv6 优化保障。</li>
+                            <li><strong><?php _e('开源维护成本：'); ?></strong><?php _e('进行版本适配、问题修复、体验优化与 LTS 支持。'); ?></li>
+                            <li><strong><?php _e('服务运行成本：'); ?></strong><?php _e('提供静态资源分发、国际/国内线路与 IPv6 优化保障。'); ?></li>
                         </ul>
                         <div class="action">
-                            <a href="https://github.com/little-gt/BooAdmin" target="_blank" rel="noopener">访问 GitHub 开源项目</a>
-                            <a href="https://cnb.cool/little-gt/BooAdmin/" class="secondary" target="_blank" rel="noopener">国内仓库</a>
+                            <a href="https://github.com/little-gt/BooAdmin" target="_blank" rel="noopener"><?php _e('访问 GitHub 开源项目'); ?></a>
+                            <a href="https://cnb.cool/little-gt/BooAdmin/" class="secondary" target="_blank" rel="noopener"><?php _e('国内仓库'); ?></a>
                         </div>
                     </div>
                     <div class="right">
                         <div class="donation-card">
-                            <img src="<?php $options->adminUrl('img/supportme.png'); ?>" alt="支持 BooAdmin 开源维护" />
+                            <img src="<?php $options->adminUrl('img/supportme.png'); ?>" alt="<?php _e('支持 BooAdmin 开源维护'); ?>" />
                         </div>
                     </div>
                 </div>
--- usr/README.md
+++ usr/README.md
@@ -1,12 +1,46 @@
-## Install
+# BooAdmin 语言包说明 / Language Pack / 言語パッケージ
+
+## 中文
+
+### 安装
+
+1. 下载最新版本并解压。
+2. 将所有 `*.mo` 文件上传到 `<你的 Typecho 目录>/usr/langs`。若该目录不存在，可在上传前自行创建。
+3. 进入 `管理后台 > 设置 > 基本设置`，下方将出现语言选择框。
+
+### 如何参与翻译
+
+1. [Fork](https://github.com/typecho/languages/fork) 本仓库并克隆到本地。
+2. 使用 [PoEdit](http://poedit.net/) 或其他 gettext 工具开始翻译。`message.pot` 文件包含所有最新的翻译字符串，但它是中文的，因此建议从英文翻译文件 `en_US.po` 入手，并从 `messages.pot` 中补全缺失的内容。
+3. 将更新推送到你 Fork 的仓库，然后向官方语言仓库 `typecho/languages` 提交 Pull Request。
+
+另请参阅 http://docs.typecho.org/translate/start
+
+## 日本語
+
+### インストール
+
+1. 最新バージョンをダウンロードし、展開してください。
+2. すべての `*.mo` ファイルを `<Typecho のディレクトリ>/usr/langs` にアップロードしてください。そのディレクトリが存在しない場合は、アップロード前に作成できます。
+3. `管理画面 > 設定 > 一般設定` に移動すると、下部に言語選択肢が表示されます。
+
+### 翻訳の方法
+
+1. このリポジトリを [フォーク](https://github.com/typecho/languages/fork) し、ローカルにクローンしてください。
+2. PoEdit またはその他の gettext ソフトウェアを使って翻訳を始めてください。`message.pot` ファイルには最新の翻訳文字列がすべて含まれていますが、中国語で書かれています。そのため、英語翻訳ファイル `en_US.po` から始め、`messages.pot` から不足しているメッセージを更新するとよいでしょう。
+3. フォークしたリポジトリに更新をプッシュし、公式言語リポジトリ `typecho/languages` へプルリクエストを送ってください。
+
+詳細は http://docs.typecho.org/translate/start もご参照ください。
+
+## English
+
+### Install
 
 1. Download the latest version and uncompress it.
-
 2. Upload all `*.mo` files to `<Your Typecho Directory>/usr/langs` . If that directory doesn't exist, you can create one before uploading.
+3. Go to `admin > options > general options`, there will be a language selector shows below.
 
-3. Go to `admin > options > genral options`, there will be a language selector shows below.
-
-## How-to
+### How-to
 
 1. [Fork](https://github.com/typecho/languages/fork) this repo and clone to local.
 2. Start your translation road by using [PoEdit](http://poedit.net/) or other gettext software. The `message.pot` file contains all recent translation strings, but it is in Chinese, so you might want to start with the English translation file `en_US.po` and update the missing messages from `messages.pot`.
